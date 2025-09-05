b312690d855d5c8e465b87b04c7bce607823e0fe
bhanu pratap <bhannupratapnagyan@gmail.com>
2025-09-06 01:36:30 +0530

    deployment ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors';
 import dotenv from "dotenv";
+import path from 'path'
 
 import noteRoutes from './routes/noteRoutes.js'
 import { connectDB } from '../config/db.js';
@@ -11,18 +12,30 @@ dotenv.config();
 
 
 const app = express();
-const PORT  = process.env.PORT || 5001
+const PORT  = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 //middleware
+if(process.env.NODE_ENV !=="production"){
 app.use(cors({
   origin:"http://localhost:5173"
-}));
+})
+);
+}
 app.use(express.json());// this middleware will parse json bodies
 app.use(rateLimiter);
 
 
 
-app.use("/api/notes",noteRoutes)
+app.use("/api/notes",noteRoutes);
+
+if(process.env.NODE_ENV ==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+  })
+}
 
 
 
